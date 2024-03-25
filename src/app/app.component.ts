e9073dd65bb356eaff75af01139251b6e42f74ec
niklas.roemer <niklas.roemer@cgi.com>
2024-03-25 13:36:29 +0100

    Pages 95-100

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,21 @@ export class AppComponent {
   articles: Article[];
   constructor(){
      this.articles = [
-      new Article('Angular 2', 'http//angular.io', 3),
-      new Article('Fullstack', 'http//fullstack.io', 2),
-      new Article('Angular Homepage', 'http//angular.io', 1),
+      new Article('Angular 2', 'https://angular.io', 3),
+      new Article('Fullstack', 'https://fullstack.io', 2),
+      new Article('Angular Homepage', 'https://angular.io', 1),
     ];
   }
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value ='';
+    link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[]{
+    return this.articles.sort((a: Article, b: Article)=> b.votes - a.votes);
   }
 
 }
